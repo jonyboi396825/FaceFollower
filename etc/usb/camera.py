1a37102fdbef0240dda2e1c4dce6c11ab5012246
Jonathan Liu <jonathanhliu21@gmail.com>
2021-12-18 11:15:26 -0800

    add face detection streaming

---
+++ etc/usb/camera.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import cv2
+
+cascade = cv2.CascadeClassifier("haarcascade_frontalface_alt2.xml")
+
+class VCam():
+    def __init__(self) -> None:
+        self.video = cv2.VideoCapture(0)
+
+    def __del__(self):
+        print("deleting")
+        self.video.release()
+
+    def next_frame(self):
+        global cascade
+            
+        _, image = self.video.read()
+
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        faces = cascade.detectMultiScale(gray, 1.3, 5)
+
+        for x, y, w, h in faces:
+            cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+        _, jpeg = cv2.imencode(".jpg", image)
+        return jpeg.tobytes()
+    
--- etc/usb/usb.py
+++ etc/usb/usb.py
@@ -1,19 +1,28 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import time
+
 from com_server import Connection, RestApiHandler, Builtins
+from flask import Flask, render_template, Response
 
-def main():
-    # order of priority
-    # first try USB0, then USB1, then ACM0, then ACM1
+from camera import VCam
 
-    conn = Connection(115200, "/dev/ttyUSB0", "/dev/ttyUSB1", "/dev/ttyACM0", "/dev/ttyACM1", send_interval=0.1)
+conn = Connection(115200, "/dev/ttyUSB0", "/dev/ttyUSB1", "/dev/ttyACM0", "/dev/ttyACM1", send_interval=0.1)
 
-    handler = RestApiHandler(conn, has_register_recall=False, add_cors=True)
-    Builtins(handler)
-     
+handler = RestApiHandler(conn, has_register_recall=False, add_cors=True, catch_all_404s=True)
+Builtins(handler)
+
+def stream_gen(cam: VCam):
+    while True:
+        frame = cam.next_frame()
+        yield (b'--frame\r\nContent-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+        time.sleep(0.01)
+
+@handler.flask_obj.route("/stream")
+def stream():
+    return Response(stream_gen(VCam()), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == "__main__": 
     handler.run_prod(host="0.0.0.0", port=7123)
-
-if __name__ == "__main__":
-    main()
 
